7db0a70d826274d4506c17bb50922e8d109448f8
Lance De Waele <lancedewaele@icloud.com>
2025-01-03 12:40:24 +0100

    rm rename

--- rename/src/lib/firebase.ts
+++
@@ -1,8 +0,0 @@
-import admin from 'firebase-admin'
-import serviceAccount from './config/FirebaseService.json'
-
-const firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-})
-
-export default firebaseAdmin
